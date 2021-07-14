20d708c676c11b77d87a90a2949291a9f8dbb2dd
Brian Park <park-brian@users.noreply.github.com>
2021-07-13 21:45:36 -0400

    update example configuration

--- configure-task.example.py
+++ configure-task.example.py
@@ -1,2 +1,19 @@
 def config():
-    return {"source": {}, "destination": {}}
+    return {
+        "cloudwatch_log_group_arn": "my-cloudwatch-log-group-arn",
+        "sns_topic_arn": "my-sns-topic-arn",
+        "source": {
+            "type": "nfs",
+            "hostname": "my-nfs-hostname",
+            "subdirectory": "/source/directory",
+            "agent_arns": [
+                "my-nfs-agent-arn"
+            ]
+        }, 
+        "destination": {
+            "type": "s3",
+            "arn": "my-s3-bucket-arn",
+            "access_role_arn": "my-s3-bucket-access-role-arn",
+            "subdirectory": "/target/directory"
+        }
+    }
